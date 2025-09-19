bc59dc06302570cf4745a553f20c22bdd17d445b
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2025-09-18 22:12:19 -0300

    fix: set swagger info:command swagger install in the readme

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
-    @Get()
+    // if make the management you need insert decorator name
+    //with decorator name "mazda" your endpoint will be cars/mazda
+    @Get('mazda')
     findAll(): string{
         return "Mazda RX7"
     }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //Set Swagger on the application
+  const config = new DocumentBuilder()
+    .setTitle('Cars exemple')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cars')
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  // 'docs' your endpoint
+  SwaggerModule.setup('docs',app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
