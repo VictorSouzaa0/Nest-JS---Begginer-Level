3e66a772f67c171acbd81829dacdc9fdc76da6e5
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2025-09-19 20:14:39 -0300

    feat: create user and auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,6 @@
-import { Controller, Post } from "@nestjs/common";
-import { AuthService } from "./auth.service";
+import { Controller } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController{
-    constructor(private authService: AuthService) {}
-
-    @Post('signup')
-    signup() {
-        return this.authService.signup();
-    };
-
-    @Post('signin')
-    signin() {
-        return this.authService.signin();
-    }
-};
+export class AuthController {
+    
+}
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
-  //Set Swagger on the application
-  const config = new DocumentBuilder()
-    .setTitle('Fisrt API exemple')
-    .setDescription('The API description')
-    .setVersion('1.0')
-    .addTag('testing')
-    .build()
-
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  // 'docs' your endpoint
-  SwaggerModule.setup('docs',app, documentFactory);
-
-  await app.listen(process.env.PORT ?? 3333);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
