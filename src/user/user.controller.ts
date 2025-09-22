95cc088bcaf793061801162f6385f40f697b9ebd
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2025-09-22 10:11:44 -0300

    fix: signup dto

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,17 @@
-import {
-Controller, Post,Body,} from '@nestjs/common';
+import {Controller, Post,Body,} from '@nestjs/common';
 import { UserService } from './user.service';
 import { Prisma, User as UserModel } from 'generated/prisma';
 import { CrateUserDTO } from './create-user.dto';
+
+
+
 @Controller('user')
 export class UserController {
     constructor(private userSerivce: UserService){}
     
     @Post('signup')
     async signupUser(
-        @Body() userData: Prisma.UserCreateInput,
+        @Body() userData: CrateUserDTO,
     ): Promise<UserModel> {
         return this.userSerivce.createUser({...userData});
     }
