a24dc8e1c42e7e4e2cee677060f3163db4bf8442
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2025-09-23 03:23:45 -0300

    feat: Hash user Password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,38 +2,44 @@ import { Injectable } from '@nestjs/common';
 import { Prisma } from 'generated/prisma';
 import { PrismaService } from 'src/database/prisma.service';
 import { User } from 'generated/prisma';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDTO } from './user.dto';
 
 @Injectable()
 export class UserService {
     constructor(private prisma: PrismaService) {}
+    async createUser(data: CreateUserDTO): Promise<User> {
 
-    
-    
-    async createUser(data:Prisma.UserCreateInput):Promise<User>{
-        
-        
+        const saltOrRounds = 10;
+        const password = data.password;
+        const hash = await bcrypt.hash(password, saltOrRounds);
 
         return this.prisma.user.create({
-            data,
-            
-        });
-    }
+            data: {
+                email: data.email,
+                password: hash,
+                username: data.username
+
+            }
+        }       
+    )
+}
 
     async updateUser(params: {
-        where: Prisma.UserWhereUniqueInput;
-        data: Prisma.UserUpdateInput;
-    }): Promise<User |  null> {
-        const {where,data} = params;
-        return this.prisma.user.update({
-            data,
-            where
-        });
-    }
+    where: Prisma.UserWhereUniqueInput;
+    data: Prisma.UserUpdateInput;
+}): Promise < User | null > {
+    const { where, data } = params;
+    return this.prisma.user.update({
+        data,
+        where
+    });
+}
 
-    async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
-        return this.prisma.user.delete({
-            where,
-        }); 
-    }
+    async deleteUser(where: Prisma.UserWhereUniqueInput): Promise < User > {
+    return this.prisma.user.delete({
+        where,
+    });
+}
 }
 
